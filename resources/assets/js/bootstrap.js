2fdbac20419c1884ced5bd4d62bc9a881335a630
DIPANKAR BACHHAR <dipit2007@gmail.com>
2017-03-22 15:52:05 +0530

    laravel Echo is ready for use

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,9 +38,14 @@ window.axios.defaults.headers.common = {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo"
+import Echo from "laravel-echo"
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '0f612b86ea53df0c6c0d',
+    cluster: 'ap2',
+    encrypted: true
+});
+
