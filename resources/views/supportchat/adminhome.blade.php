7bb8da441f287639900ceb010be22f21adf91283
DIPANKAR BACHHAR <dipit2007@gmail.com>
2017-03-31 08:57:57 +0530

    Support Chat Channels List for Admin User

--- resources/views/supportchat/adminhome.blade.php
+++ resources/views/supportchat/adminhome.blade.php
@@ -14,32 +14,4 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Support Chat Messages</div>
-
-                <div class="panel-body" style="max-height: 300px;overflow: auto;">
-                    <supportchatmessages :user="{{ auth()->user() }}"></supportchatmessages>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Send Message</div>
-                <div class="panel-body">
-                    <supportchatmessageform
-                        v-on:messagesent="addNewSupportChatMessageFromUser"
-                        :user="{{ Auth::user() }}"
-                    ></supportchatmessageform>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
